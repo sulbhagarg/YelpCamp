899325cfec53743d9b7937db4d36fcdabeff9213
sulbhagarg <43903470+sulbhagarg@users.noreply.github.com>
2020-06-15 01:02:31 +0530

    Updated comments in app.js for better understanding

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// ======================
+// Classic Node-Packages
+// ======================
 var express        = require("express"),
     app            = express(),
     bodyParser     = require("body-parser"),
@@ -6,19 +9,32 @@ var express        = require("express"),
     passport       = require("passport"),
     LocalStrategy  = require("passport-local"),
     methodOverride = require("method-override"),
-    Campground     = require("./models/campground"),
-    Comment        = require("./models/comment"),
-    User           = require("./models/user"),
-    seedDb         = require("./seeds"),
-    PORT           = process.env.PORT || 3000,
+    seedDb         = require("./seeds");
+
+// ========================================
+// Setting up the environment variables.
+// ========================================
+var PORT           = process.env.PORT || 3000,
     URL            = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
 
+// ==================
 // Requiring routes
+// ==================
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index"),
     profileRoute     = require("./routes/profile");
 
+// ======================
+// Importing the models
+// ======================
+var Campground     = require("./models/campground"),
+    Comment        = require("./models/comment"),
+    User           = require("./models/user");
+
+// ==============================
+// Connection setup to database
+// ==============================
 mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log("mongodb is connected");
 }).catch((error)=>{
@@ -26,14 +42,39 @@ mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(
     console.log(error);
 });
 
+// =============================
+// Body Parser for JSON format
+// =============================
 app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine","ejs");
-app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
-app.use(flash());
-// seedDb(); // seed the database     
 
+// ===========================
+// Setting up the view engine
+// ===========================
+app.set("view engine","ejs");
+
+// ======================
+// All the static files
+// ======================
+app.use(express.static(__dirname + "/public"));
+
+// ====================
+// For RESTful Routing
+// ====================
+app.use(methodOverride("_method"));
+
+// ===========================
+// For prompting the messages 
+// ===========================
+app.use(flash());
+
+// ===============================================
+// This is to seed the database for testing purpose
+// ===============================================
+// seedDb();    
+
+// ===========================
 // PASSPORT CONFIGURATION
+// ===========================
 app.use(require("express-session")({
     secret: "Here I am, with my first website.",
     resave: false,
@@ -45,6 +86,9 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// =============================
+// Setting up the user globally
+// =============================
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
     res.locals.error = req.flash("error");
@@ -52,12 +96,17 @@ app.use(function(req, res, next){
     next();
 });
 
+// ======================
+// Routes specifications
+// ======================
 app.use("/", indexRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/profile", profileRoute);
 
+// ==============================================
+// The very important of all, PORT listener!!
+// ==============================================
 app.listen(PORT, function(){
     console.log("The YelpCamp Server Has Started!");
-    // console.log(DATABASEURL);
 });
